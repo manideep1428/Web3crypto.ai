9c3df6c97312663f82733b39b020fd0516439100
manideep1428 <saimanideep.ch12345@gmail.com>
2024-09-28 00:41:44 +0530

    Added features

--- app/(server)/api/route.ts
+++ app/(server)/api/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse} from "next/server";
 
-export async function GET(request) {
+export async function GET(request: Request) {
   return NextResponse.json({
     message: "Server is Running",
   });
